7a8b42e078d4722a8ebec5881d1cbc3c0e9f4b44
tstuart <tstuart@beckshybrids.com>
2015-09-28 15:10:22 -0400

    fixed error, added message to NotesController

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,3 +1,27 @@
 /**
  * Created by tstuart on 9/28/15.
  */
+(function() {
+    angular.module('notely.notes', [
+        'ui.router'
+    ])
+        .controller('NotesController', NotesController)
+        .config(notesConfig);
+
+    notesConfig['$inject'] = ['$stateProvider'];
+    function notesConfig($stateProvider) {
+
+        $stateProvider
+
+            .state('notes', {
+                url: '/notes',
+                templateUrl: '/notes/notes.html',
+                controller: NotesController
+            });
+    }
+
+    function NotesController($scope) {
+        $scope.message = "Hello, Notely";
+    }
+
+})();
