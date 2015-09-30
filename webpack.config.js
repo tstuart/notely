cc4833db3563bb9510bef4d62fac96429f57b22f
tstuart <tstuart@beckshybrids.com>
2015-09-30 16:45:20 -0400

    attempt at webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./app/app.js",
     output: {
-        path: __dirname + 'app/dist',
+        path: __dirname + '/app/dist',
         filename: "bundle.js"
     },
     module: {
