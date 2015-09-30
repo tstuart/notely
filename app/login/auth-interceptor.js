24311d30f7dff924f769df98071feefad3657b2a
tstuart <tstuart@beckshybrids.com>
2015-09-30 11:33:00 -0400

    add token to header

--- app/login/auth-interceptor.js
+++ app/login/auth-interceptor.js
@@ -6,9 +6,14 @@
     angular.module('notely.login')
         .factory('AuthInterceptor', AuthInterceptor);
 
-    function AuthInterceptor() {
+    AuthInterceptor['$inject'] = ['AuthToken'];
+    function AuthInterceptor(AuthToken) {
         return {
             request: function(config) {
+                var token = AuthToken.get();
+                if (token) {
+                    config.headers['Authorization'] = token;
+                }
                 return config;
             }
         };
